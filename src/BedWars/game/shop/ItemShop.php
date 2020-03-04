8c6a600a18dc9100ea60396777ab0461e94ca40b
PQguanfang <35395058+PQguanfang@users.noreply.github.com>
2020-03-04 09:20:59 +0800

    Update ItemShop.php

--- src/BedWars/game/shop/ItemShop.php
+++ src/BedWars/game/shop/ItemShop.php
@@ -25,44 +25,44 @@ class ItemShop
      * @var array $shopWindows
      */
     public static $shopWindows = [
-        1 => ["name" =>"§l§aArmor", "image" => "https://www.spigotmc.org/attachments/ftiroac-png.241966/"],
-        2 => ["name" =>"§l§aWeapons", "image" => "http://icons.iconarchive.com/icons/chrisl21/minecraft/512/Stone-Sword-icon.png"],
-        3 => ["name" =>"§l§aBlocks", "image" => "https://d1u5p3l4wpay3k.cloudfront.net/minecraft_gamepedia/0/07/White_Wool.png"],
-        4 => ["name" => "§l§aBows", "image" => "https://lh3.googleusercontent.com/MS2w4Tlmw4azfmxVcT9o29cq74YgMau26xni5DiqbzpxHFMIEpHtPxdGWWZlV-WD0oXNLUXsiKs2W2yAzMT0=s400"],
-        5 => ["name" => "§l§aPotions" ,"image" => "http://www.blocksandgold.com//media/catalog/product/cache/3/image/200x/6cffa5908a86143a54dc6ad9b8a7c38e/s/p/splash_red.png"],
-        6 => ["name" => "§l§aSpecials", "image" => "https://d1u5p3l4wpay3k.cloudfront.net/minecraft_gamepedia/1/1e/TNT.png"]
+        1 => ["name" => "§l§a盔甲", "image" => "https://www.spigotmc.org/attachments/ftiroac-png.241966/"],
+        2 => ["name" => "§l§a武器", "image" => "http://icons.iconarchive.com/icons/chrisl21/minecraft/512/Stone-Sword-icon.png"],
+        3 => ["name" => "§l§a方块", "image" => "https://d1u5p3l4wpay3k.cloudfront.net/minecraft_gamepedia/0/07/White_Wool.png"],
+        4 => ["name" => "§l§a弓", "image" => "https://lh3.googleusercontent.com/MS2w4Tlmw4azfmxVcT9o29cq74YgMau26xni5DiqbzpxHFMIEpHtPxdGWWZlV-WD0oXNLUXsiKs2W2yAzMT0=s400"],
+        5 => ["name" => "§l§a药水" ,"image" => "http://www.blocksandgold.com//media/catalog/product/cache/3/image/200x/6cffa5908a86143a54dc6ad9b8a7c38e/s/p/splash_red.png"],
+        6 => ["name" => "§l§a道具", "image" => "https://d1u5p3l4wpay3k.cloudfront.net/minecraft_gamepedia/1/1e/TNT.png"]
     ];
 
     /**
      * @var array $shopPages
      */
     public static $shopPages = [
-        0 => ["§6Chainmal Armor\n§l§e40 IRON" => ["image" => "https://www.spigotmc.org/attachments/ftiroac-png.241966/"],
-            "§6Iron Armor §c[PERMANENT]\n§l§e12 GOLD" => ["image" => "http://www.minecraftopia.com/images/blocks/iron_boots.png"],
-            "§6Diamond Armor §c[PERMANENT]\n§l§e6 EMERALD" => ["http://static.wixstatic.com/media/34c645_d8e567b9ae8645fa98e0775d45493df3.png"]],
-        1 => ["§6Stone Sword\n§l§e10 IRON" => ["image" => "http://iconbug.com/data/0c/256/b86a957742c00c3804bd0fa293c98cde.png"],
-            "§6Iron Sword\n§l§e7 GOLD" => ["image" => "https://vignette.wikia.nocookie.net/animaljam/images/2/27/Iron-Sword-icon.png/revision/latest/scale-to-width-down/480?cb=20141108192633"],
-            "§6Diamond Sword\n§l§e7 EMERALD" => ["image" => "http://www.freepngimg.com/download/minecraft/16-2-diamond-sword-minecraft-png.png"],
-            "§6Knockback Stick\n§l§e2 EMERALD" => ["image" => "https://vignette.wikia.nocookie.net/thetekkit/images/c/c4/Obsidian_Stick.png/revision/latest?cb=20121011074642"]],
-        2 => ["§6Wool 16x\n§l§e4 IRON" => ["image" => "http://d1u5p3l4wpay3k.cloudfront.net/minecraft_gamepedia/0/07/White_Wool.png"],
-            "§6Sandstone 16x\n§l§e12 IRON" => ["image" => "http://d1u5p3l4wpay3k.cloudfront.net/minecraft_gamepedia/d/d6/Sandstone.png"],
-            "§6Endstone 12x\n§l§e24 IRON" => ["image" => "http://www.minecraftguides.org/blocks/endstone.png"],
-            "§6Ladder 16x\n§l§e4 IRON" => ["image" => "https://hydra-media.cursecdn.com/minecraft.gamepedia.com/archive/6/63/20101021024334!Ladder.png"],
-            "§6Oak Wood 16x\n§l§e4 GOLD" => ["image" => "https://d1u5p3l4wpay3k.cloudfront.net/minecraft_gamepedia/0/0f/Oak_Wood_Planks.png"],
-            "§6Obsidian 4x\n§l§e4 EMERALD" => ["image" => "https://d1u5p3l4wpay3k.cloudfront.net/minecraft_gamepedia/2/23/Obsidian.png"]],
-        3 => ["§6Normal Bow\n§l§e12 GOLD" => ["image" => "https://d1u5p3l4wpay3k.cloudfront.net/minecraft_gamepedia/6/65/Bow.png"],
-            "§6Bow §b(Power 1)\n§l§e24 GOLD" => ["image" => "https://i.imgur.com/MWw3yIJ.png"],
-            "§6Bow §b(Power 1, Punch 1)\n§l§e6 EMERALD" => ["image" => "https://i.imgur.com/MWw3yIJ.png"]],
-        4 => ["§6Speed II Potion (45 sec.)\n§l§e1 EMERALD" => ["image" => "http://www.blocksandgold.com//media/catalog/product/cache/3/image/200x/6cffa5908a86143a54dc6ad9b8a7c38e/p/o/potion_light_blue.png"],
-            "§6Jump V Potion (45 sec.)\n§l§e1 EMERALD" => ["http://www.minecraftopia.com/images/blocks/potion_of_leaping.png"],
-            "§6Invisibility Potion (30 sec.)\n§l§e1 EMERALD" => ["https://vignette.wikia.nocookie.net/minecraft-computer/images/7/78/Potion_blue.png/revision/latest?cb=20130701150754"]],
-        5 => ["§6Golden Apple\n§l§e3 GOLD" => ["image" => "https://d1u5p3l4wpay3k.cloudfront.net/minecraft_gamepedia/0/0e/Golden_Apple.png"],
-            "§6Bedbug\n§l§e50 IRON" => ["image" => "https://d1u5p3l4wpay3k.cloudfront.net/minecraft_gamepedia/0/04/Snowball.png"],
-            "§6Fireball\n§l§e50 IRON" => ["image" => "https://d1u5p3l4wpay3k.cloudfront.net/minecraft_gamepedia/9/98/Fire_Charge.png"],
-            "§6TNT\n§l§e8 GOLD" => ["image" => "https://d1u5p3l4wpay3k.cloudfront.net/minecraft_gamepedia/1/1e/TNT.png"],
-            "§6Enderpearl\n§l§e4 EMERALD" => ["image" => "https://d1u5p3l4wpay3k.cloudfront.net/minecraft_gamepedia/5/5a/Ender_Pearl.png"],
-            "§6Water Bucker\n§l§e1 EMERALD" => ["image" => "https://vignette.wikia.nocookie.net/minecraftpocketedition/images/d/d3/Water_Bucket.png/revision/latest/fixed-aspect-ratio-down/width/320/height/320?cb=20141004050736&fill=transparent"],
-            "§6Egg\n§l§e4 EMERALD" => ["image" => "https://d1u5p3l4wpay3k.cloudfront.net/minecraft_gamepedia/2/26/Egg.png"]
+        0 => ["§6锁链甲\n§l§e40 铁" => ["image" => "https://www.spigotmc.org/attachments/ftiroac-png.241966/"],
+            "§6铁甲 §c[PERMANENT]\n§l§e12 金" => ["image" => "http://www.minecraftopia.com/images/blocks/iron_boots.png"],
+            "§6钻石甲 §c[PERMANENT]\n§l§e6 绿宝石" => ["http://static.wixstatic.com/media/34c645_d8e567b9ae8645fa98e0775d45493df3.png"]],
+        1 => ["§6石剑\n§l§e10 铁" => ["image" => "http://iconbug.com/data/0c/256/b86a957742c00c3804bd0fa293c98cde.png"],
+            "§6铁剑\n§l§e7 金" => ["image" => "https://vignette.wikia.nocookie.net/animaljam/images/2/27/Iron-Sword-icon.png/revision/latest/scale-to-width-down/480?cb=20141108192633"],
+            "§6钻石剑\n§l§e7 绿宝石" => ["image" => "http://www.freepngimg.com/download/minecraft/16-2-diamond-sword-minecraft-png.png"],
+            "§6击退棒\n§l§e2 绿宝石" => ["image" => "https://vignette.wikia.nocookie.net/thetekkit/images/c/c4/Obsidian_Stick.png/revision/latest?cb=20121011074642"]],
+        2 => ["§6羊毛 16x\n§l§e4 铁" => ["image" => "http://d1u5p3l4wpay3k.cloudfront.net/minecraft_gamepedia/0/07/White_Wool.png"],
+            "§6沙石 16x\n§l§e12 铁" => ["image" => "http://d1u5p3l4wpay3k.cloudfront.net/minecraft_gamepedia/d/d6/Sandstone.png"],
+            "§6末地石 12x\n§l§e24 铁" => ["image" => "http://www.minecraftguides.org/blocks/endstone.png"],
+            "§6梯子 16x\n§l§e4 铁" => ["image" => "https://hydra-media.cursecdn.com/minecraft.gamepedia.com/archive/6/63/20101021024334!Ladder.png"],
+            "§6橡木 16x\n§l§e4 金" => ["image" => "https://d1u5p3l4wpay3k.cloudfront.net/minecraft_gamepedia/0/0f/Oak_Wood_Planks.png"],
+            "§6黑曜石 4x\n§l§e4 绿宝石" => ["image" => "https://d1u5p3l4wpay3k.cloudfront.net/minecraft_gamepedia/2/23/Obsidian.png"]],
+        3 => ["§6普通弓\n§l§e12 金" => ["image" => "https://d1u5p3l4wpay3k.cloudfront.net/minecraft_gamepedia/6/65/Bow.png"],
+            "§6弓 §b(力量 1)\n§l§e24 金" => ["image" => "https://i.imgur.com/MWw3yIJ.png"],
+            "§6弓 §b(力量 1, 击退 1)\n§l§e6 绿宝石" => ["image" => "https://i.imgur.com/MWw3yIJ.png"]],
+        4 => ["§6速度 II 药水 (45 秒)\n§l§e1 绿宝石" => ["image" => "http://www.blocksandgold.com//media/catalog/product/cache/3/image/200x/6cffa5908a86143a54dc6ad9b8a7c38e/p/o/potion_light_blue.png"],
+            "§6跳跃提升 V 药水 (45 秒)\n§l§e1 绿宝石" => ["http://www.minecraftopia.com/images/blocks/potion_of_leaping.png"],
+            "§6隐身药水 (30 秒)\n§l§e1 绿宝石" => ["https://vignette.wikia.nocookie.net/minecraft-computer/images/7/78/Potion_blue.png/revision/latest?cb=20130701150754"]],
+        5 => ["§6金苹果\n§l§e3 金" => ["image" => "https://d1u5p3l4wpay3k.cloudfront.net/minecraft_gamepedia/0/0e/Golden_Apple.png"],
+            "§6生成 蠹虫\n§l§e50 铁" => ["image" => "https://d1u5p3l4wpay3k.cloudfront.net/minecraft_gamepedia/0/04/Snowball.png"],
+            "§6火球\n§l§e50 铁" => ["image" => "https://d1u5p3l4wpay3k.cloudfront.net/minecraft_gamepedia/9/98/Fire_Charge.png"],
+            "§6TNT\n§l§e8 金" => ["image" => "https://d1u5p3l4wpay3k.cloudfront.net/minecraft_gamepedia/1/1e/TNT.png"],
+            "§6末影珍珠\n§l§e4 绿宝石" => ["image" => "https://d1u5p3l4wpay3k.cloudfront.net/minecraft_gamepedia/5/5a/Ender_Pearl.png"],
+            "§6Water Bucker\n§l§e1 绿宝石" => ["image" => "https://vignette.wikia.nocookie.net/minecraftpocketedition/images/d/d3/Water_Bucket.png/revision/latest/fixed-aspect-ratio-down/width/320/height/320?cb=20141004050736&fill=transparent"],
+            "§6Egg\n§l§e4 绿宝石" => ["image" => "https://d1u5p3l4wpay3k.cloudfront.net/minecraft_gamepedia/2/26/Egg.png"]
         ],
     ];
 
@@ -70,25 +70,25 @@ class ItemShop
      * @var array $itemData
      */
     public static $itemData = [
-        0 => [0 => ["name" => "Chainmal Armor", "type" => self::PURCHASE_TYPE_IRON, "amount" => 0, "price" => 40, "item" => ["id" => Item::CHAIN_LEGGINGS, "damage" => 0]],
-            1 => ["name" => "Iron Armor", "type" => self::PURCHASE_TYPE_GOLD, "amount" => 0, "price" => 12, "item" => ["id" => Item::IRON_LEGGINGS, "damage" => 0]],
-            2 => ["name" => "Diamond Armor ", "type" => self::PURCHASE_TYPE_EMERALD, "amount" => 0, "price" => 6, "item" => ["id" => ITEM::DIAMOND_LEGGINGS, "damage" => 0]]
+        0 => [0 => ["name" => "锁链甲", "type" => self::PURCHASE_TYPE_IRON, "amount" => 0, "price" => 40, "item" => ["id" => Item::CHAIN_LEGGINGS, "damage" => 0]],
+            1 => ["name" => "铁甲", "type" => self::PURCHASE_TYPE_GOLD, "amount" => 0, "price" => 12, "item" => ["id" => Item::IRON_LEGGINGS, "damage" => 0]],
+            2 => ["name" => "钻石甲", "type" => self::PURCHASE_TYPE_EMERALD, "amount" => 0, "price" => 6, "item" => ["id" => ITEM::DIAMOND_LEGGINGS, "damage" => 0]]
         ],
-        1 => [0 => ["name" => "Stone Sword", "type" => self::PURCHASE_TYPE_IRON, "amount" => 1, "price" => 10, "item" => ["id" => Item::STONE_SWORD, "damage" => 0]],
-            1 => ["name" => "Iron Sword", "type" => self::PURCHASE_TYPE_GOLD, "amount" => 1, "price" => 7, "item" => ["id" => Item::IRON_SWORD, "damage" => 0]],
-            2 => ["name" => "Diamond Sword", "type" => self::PURCHASE_TYPE_EMERALD, "amount" => 1, "price" => 7, "item" => ["id" => Item::DIAMOND_SWORD, "damage" => 0]],
-            3 => ["name" => "Knockback Stick", "type" => self::PURCHASE_TYPE_EMERALD, "amount" => 1, "price" => 2, "item" => ["id" => Item::STICK, "damage" => 0]]
+        1 => [0 => ["name" => "石剑", "type" => self::PURCHASE_TYPE_IRON, "amount" => 1, "price" => 10, "item" => ["id" => Item::STONE_SWORD, "damage" => 0]],
+            1 => ["name" => "铁剑", "type" => self::PURCHASE_TYPE_GOLD, "amount" => 1, "price" => 7, "item" => ["id" => Item::IRON_SWORD, "damage" => 0]],
+            2 => ["name" => "钻石剑", "type" => self::PURCHASE_TYPE_EMERALD, "amount" => 1, "price" => 7, "item" => ["id" => Item::DIAMOND_SWORD, "damage" => 0]],
+            3 => ["name" => "击退棒", "type" => self::PURCHASE_TYPE_EMERALD, "amount" => 1, "price" => 2, "item" => ["id" => Item::STICK, "damage" => 0]]
         ],
-        2 => [0 => ["name" => "Wool 16x", "type" => self::PURCHASE_TYPE_IRON, "amount" => 16, "price" => 4, "item" => ["id" => Item::WOOL, "damage" => "depend"]],
-            1 => ["name" => "Sandstone 16x", "type" => self::PURCHASE_TYPE_IRON, "amount" => 16, "price" => 12, "item" => ["id" => Item::SANDSTONE, "damage" => 0]],
-            2 => ["name" => "Endstone 12x", "type" => self::PURCHASE_TYPE_IRON, "amount" => 12, "price" => 24,"item" => ["id" => Item::END_STONE, "damage" => 0]],
-            3 => ["name" => "Ladder 16x", "type" => self::PURCHASE_TYPE_IRON, "amount" => 16, "price" => 4,"item" => ["id" => Item::LADDER, "damage" => 0]],
-            4 => ["name" => "Oak Wood 16x", "type" => self::PURCHASE_TYPE_GOLD, "amount" => 16, "price" => 4, "item" => ["id" => 5, "damage" => 0]],
-            5 => ["name" => "Obsidian 4x", "type" => self::PURCHASE_TYPE_EMERALD, "amount" => 4, "price" => 4, "item" => ["id" => Item::OBSIDIAN, "damage" =>0]]
+        2 => [0 => ["name" => "羊毛 16x", "type" => self::PURCHASE_TYPE_IRON, "amount" => 16, "price" => 4, "item" => ["id" => Item::WOOL, "damage" => "depend"]],
+            1 => ["name" => "沙石 16x", "type" => self::PURCHASE_TYPE_IRON, "amount" => 16, "price" => 12, "item" => ["id" => Item::SANDSTONE, "damage" => 0]],
+            2 => ["name" => "末地石 12x", "type" => self::PURCHASE_TYPE_IRON, "amount" => 12, "price" => 24,"item" => ["id" => Item::END_STONE, "damage" => 0]],
+            3 => ["name" => "梯子 16x", "type" => self::PURCHASE_TYPE_IRON, "amount" => 16, "price" => 4,"item" => ["id" => Item::LADDER, "damage" => 0]],
+            4 => ["name" => "橡木 16x", "type" => self::PURCHASE_TYPE_GOLD, "amount" => 16, "price" => 4, "item" => ["id" => 5, "damage" => 0]],
+            5 => ["name" => "黑曜石 4x", "type" => self::PURCHASE_TYPE_EMERALD, "amount" => 4, "price" => 4, "item" => ["id" => Item::OBSIDIAN, "damage" =>0]]
         ],
-        3 => [0 => ["name" => "Bow 1", "type" => self::PURCHASE_TYPE_GOLD, "amount" => 1, "price" => 12, "item" => ["id" => Item::BOW, "damage" => 0]],
-            1 => ["name" => "Bow 2", "type" => self::PURCHASE_TYPE_GOLD, "amount" => 1, "price" => 24, "item" => ["id" => Item::BOW, "damage" => 0]],
-            2 => ["name" => "Bow 3", "type" => self::PURCHASE_TYPE_EMERALD, "amount" => 1, "price" => 6, "item" => ["id" => Item::BOW, "damage" => 0]]
+        3 => [0 => ["name" => "弓 1", "type" => self::PURCHASE_TYPE_GOLD, "amount" => 1, "price" => 12, "item" => ["id" => Item::BOW, "damage" => 0]],
+            1 => ["name" => "弓 2", "type" => self::PURCHASE_TYPE_GOLD, "amount" => 1, "price" => 24, "item" => ["id" => Item::BOW, "damage" => 0]],
+            2 => ["name" => "弓 3", "type" => self::PURCHASE_TYPE_EMERALD, "amount" => 1, "price" => 6, "item" => ["id" => Item::BOW, "damage" => 0]]
         ],
         4 => [0 => ["name" => "Potion of Speed", "type" => self::PURCHASE_TYPE_EMERALD, "amount" => 1, "price" => 1, "item" => ["id" => Item::POTION, "damage" => 8194]],
             1 => ["name" => "Jump Potion", "type" => self::PURCHASE_TYPE_EMERALD, "amount" => 1, "price" => 1, "item" => ["id" => Item::POTION, "damage" => 8203]],
